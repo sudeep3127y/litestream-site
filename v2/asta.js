3207297f3581e27833fd19a980c69ecf04f3bbfa
sudeep3127y <135289561+sudeep3127y@users.noreply.github.com>
2024-05-30 17:26:40 +0545

    Delete v2 directory
    Signed-off-by: sudeep3127y <135289561+sudeep3127y@users.noreply.github.com>

--- v2/asta.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Handle login form submission
-});
-
-document.getElementById('register-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    // Handle registration form submission
-});
--- v2/js/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('emailForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var email = document.getElementById('email').value;
-    // You can add your own logic here to validate the email and show the content
-    document.getElementById('message').textContent = 'Thank you! You can now proceed to watch the content.';
-  });
