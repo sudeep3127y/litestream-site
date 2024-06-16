cba054fd76a913a56dfbf5267eed076b6a936ba3
sudeep3127y <135289561+sudeep3127y@users.noreply.github.com>
2024-06-16 17:38:24 +0545

    Update anime.js
    Signed-off-by: sudeep3127y <135289561+sudeep3127y@users.noreply.github.com>

--- v2/js/anime.js
+++ v2/js/anime.js
@@ -1,7 +1,6 @@
 const ProxyApi = "https://proxy.sb543267gmailcom.workers.dev/?u=";
-    animeapi = "/anime/"
-    recommendationsapi = "/recommendations/"
-    upcomingapi = "/upcoming/"
+    animeapi = "/anime/";
+    recommendationsapi = "/recommendations/";
 
     AvailableServers = ["https://asta-api.sb543267gmailcom.workers.dev/","https://vip-gamma.vercel.app/" ,"https://api3.sb543267gmailcom.workers.dev/","https://api100.sb543267gmailcom.workers.dev/","https://api1.sb543267gmailcom.workers.dev/"];
 function getApiServer() {
